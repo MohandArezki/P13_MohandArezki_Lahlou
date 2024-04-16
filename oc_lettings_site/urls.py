cdcfc5b14400cd67084c0840e83b1ce43bf53a28
lahlou <lahlou.mohand.arezki@gmail.com>
2024-04-16 17:08:00 +0200

    07- Move URLs for 'lettings', 'profiles', 'lettings_index', and 'profiles_index' from 'oc_lettings_site' to the new applications while keeping the ROOT_URLCONF unchanged.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,20 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
-# import the view from "lettings" app
-from lettings import views as views_lettings
-# import the view from "profiles" app
-from profiles import views as views_profiles
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # redirect the url to the "lettings_index" view in "lettings" app
-    path('lettings/', views_lettings.lettings_index, name='lettings_index'),
-    # redirect the url to the "letting" view in "lettings" app     
-    path('lettings/<int:letting_id>/', views_lettings.letting, name='letting'),
-    path('profiles/', views_profiles.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views_profiles.profile, name='profile'),
+    path('lettings/', include('lettings.urls')),
+    path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
